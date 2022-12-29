d3979647deff4280840023466bdcc2202a33ae9f
mohityede <mohityede755@gmail.com>
2022-12-29 14:44:28 +0530

    codeforces folder added

--- main.cpp
+++ main.cpp
@@ -4,16 +4,5 @@ using namespace std;
 
 int main()
 {
-  int node, edges;
-  cin >> node >> edges;
-  vector<int> adjList[node];
-  for (int i = 0; i < edges; i++)
-  {
-    int num1, num2;
-    cin >> num1 >> num2;
-    adjList[num1].push_back(num2);
-    adjList[num2].push_back(num1);
-  }
-  printAdjucencyList(adjList, node);
   return 0;
 }
